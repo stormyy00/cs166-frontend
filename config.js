463245f482cd98a4a7a4fafb604c8e75981a0573
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-06-10 16:11:40 -0700

    test prod

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-  host: process.env.NEXT_PGSQL_HOST,
-  port: process.env.NEXT_PGSQL_PORT,
-  user: process.env.NEXT_PGSQL_USER,
-  password: process.env.NEXT_PGSQL_PASSWORD,
-  database: process.env.NEXT_PGSQL_DATABASE,
+  // host: process.env.NEXT_PGSQL_HOST,
+  // port: process.env.NEXT_PGSQL_PORT,
+  // user: process.env.NEXT_PGSQL_USER,
+  // password: process.env.NEXT_PGSQL_PASSWORD,
+  // database: process.env.NEXT_PGSQL_DATABASE,
 
-  // connectionString: process.env.NEXT_PUBLIC_SQL_DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
     // ssl: {
     //   rejectUnauthorized: true,
     //   ca: process.env.NEXT_PUBLIC_SQL_CERT,
